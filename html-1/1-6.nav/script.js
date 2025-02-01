9fc48f0fc6a80f0327d4c2164c87e75c1a51371c
Koras02 <wjdgh0727@gmail.com>
2025-02-01 23:12:32 +0900

    5.html nav-ol-option-span-strong tag

---
+++ html-1/1-6.nav/script.js
@@ -0,0 +1,7 @@
+document.getElementById('btn').addEventListener("click", function() {
+    window.location.href = "https://www.naver.com"
+})
+
+document.getElementById('webHTML').addEventListener("click", function() {
+    window.location.href = "https://thinky.tistory.com/category/%EC%9B%B9/HTML"
+})
